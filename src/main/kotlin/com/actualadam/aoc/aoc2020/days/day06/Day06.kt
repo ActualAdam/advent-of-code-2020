4b3db9e0f247a7007421063b3e29c611f32f1455
Adam Miller <adam.miller@vevo.com>
2020-12-08 21:39:01 -0800

    day 6 working

--- src/main/kotlin/com/actualadam/aoc/aoc2020/days/day06/Day06.kt
+++ src/main/kotlin/com/actualadam/aoc/aoc2020/days/day06/Day06.kt
@@ -2,25 +2,37 @@ package com.actualadam.aoc.aoc2020.days.day06
 
 import com.actualadam.aoc.aoc2020.InputReader
 
-private tailrec fun parseAnswerGroups(acc: List<Set<Char>> = listOf(), input: List<String>): List<Set<Char>> {
+private tailrec fun parseAnswerGroups(input: List<String>, acc: List<List<String>> = listOf()): List<List<String>> {
     if (input.isEmpty() || input.all { it.isEmpty() }) {
         return acc
     }
-    // burn leading empty line
-    val rest = input.dropWhile { it.isEmpty() }
-    // records are separated by empty lines.
-    val answerGroup = rest.takeWhile { it.isNotEmpty() }
-    val nextInput = rest - answerGroup
-    return parseAnswerGroups(acc.plusElement(answerGroup.joinToString("").toSet()), nextInput)
+    val allAnswers = input.dropWhile { it.isEmpty() } // burn a possible leading empty line
+    val answerGroup = allAnswers.takeWhile { it.isNotEmpty() }
+    val rest = allAnswers.dropWhile { it.isNotEmpty() }
+    return parseAnswerGroups(rest, acc.plusElement(answerGroup))
 }
 
-private fun part1(puzzleInput: List<String>): Int {
-    return parseAnswerGroups(input = puzzleInput).map {it.count()}.sum()
+private fun calculateGroupAnswerCounts(puzzleInput: List<String>, reducer: (Set<Char>, Set<Char>) -> Set<Char>) : Int {
+    return parseAnswerGroups(input = puzzleInput).map { group ->
+        group.map { it.toSet() }
+            .reduce(reducer)
+            .count()
+    }.sum()
+}
 
+private fun union() = { acc:Set<Char>, str: Set<Char> -> acc.union(str) }
+private fun intersect() = { acc: Set<Char>, str: Set<Char> -> acc.intersect(str) }
+
+private fun part1(puzzleInput: List<String>): Int {
+    return calculateGroupAnswerCounts(puzzleInput, union())
+}
+
+private fun part2(puzzleInput: List<String>): Int {
+    return calculateGroupAnswerCounts(puzzleInput, intersect())
 }
 
 fun main() {
     val puzzleInput = InputReader.lines(6)
-    println(puzzleInput.count())
     println("Day 06, Part 1: ${part1(puzzleInput)}")
+    println("Day 06, Part 2: ${part2(puzzleInput)}")
 }
--- src/main/kotlin/com/actualadam/aoc/aoc2020/lib.kt
+++ src/main/kotlin/com/actualadam/aoc/aoc2020/lib.kt
@@ -1,13 +1,15 @@
 package com.actualadam.aoc.aoc2020
 
 import java.io.File
-import java.nio.file.Paths
 
 object InputReader {
-   fun lines(day: Int): List<String> {
-       val dayString = day.toString().padStart(2, '0')
-       return File(javaClass.getResource("./days/day$dayString/input.txt").toURI()).useLines {
+    fun lines(day: Int): List<String> {
+        return file(day).useLines {
             it.toList()
         }
-   }
+    }
+
+    fun file(day: Int) = File(javaClass.getResource("./days/day${formatDay(day)}/input.txt").toURI())
+
+    private fun formatDay(day: Int) = day.toString().padStart(2, '0')
 }
