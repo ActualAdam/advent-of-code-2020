77ebef58b47d0b905bf5adf8daeec1013e6b86d6
Adam Miller <adam.miller@vevo.com>
2020-12-30 21:42:23 -0800

    day 11 part 1

--- src/main/kotlin/com/actualadam/aoc/aoc2020/days/day11/Day11.kt
+++ src/main/kotlin/com/actualadam/aoc/aoc2020/days/day11/Day11.kt
@@ -1,2 +1,81 @@
 package com.actualadam.aoc.aoc2020.days.day11
 
+import com.actualadam.aoc.aoc2020.InputReader
+
+data class DeckPosition(val row: Int, val col: Int, val state: DeckPositionState)
+
+enum class DeckPositionState(val display: Char) {
+    OccupiedSeat('#'),
+    EmptySeat('L'),
+    Floor('.');
+
+    companion object {
+        private val statesByDisplay = DeckPositionState.values().associateBy { it.display }
+        fun parse(display: Char) = statesByDisplay[display] ?: throw IllegalArgumentException("no state: $display")
+    }
+}
+
+fun findByCoordinates(layout: List<DeckPosition>, row: Int, col: Int): DeckPosition? =
+    layout.find { it.row == row && it.col == col }
+
+fun parseInput(input: List<String>): List<DeckPosition> {
+    return input.flatMapIndexed { rowNum, row ->
+        row.mapIndexed { colNum, charValue ->
+            DeckPosition(rowNum, colNum, DeckPositionState.parse(charValue))
+        }
+    }
+}
+
+fun buildGraph(layout: List<DeckPosition>): Map<DeckPosition, List<DeckPosition>> {
+    return layout.map {
+        val southRow = it.row + 1
+        val northRow = it.row - 1
+        val eastCol = it.col + 1
+        val westCol = it.col - 1
+
+        val adjacents = listOf(
+            findByCoordinates(layout, northRow, westCol),
+            findByCoordinates(layout, northRow, it.col),
+            findByCoordinates(layout, northRow, eastCol),
+            findByCoordinates(layout, it.row, westCol),
+            findByCoordinates(layout, it.row, eastCol),
+            findByCoordinates(layout, southRow, westCol),
+            findByCoordinates(layout, southRow, it.col),
+            findByCoordinates(layout, southRow, eastCol),
+        ).filterNotNull()
+        Pair(it, adjacents)
+    }.toMap()
+}
+
+fun reseat(layout: List<DeckPosition>): List<DeckPosition> {
+    val graph = buildGraph(layout)
+    return layout.map { pos ->
+        when (pos.state) {
+            DeckPositionState.EmptySeat -> if (graph[pos]!!.none { it.state == DeckPositionState.OccupiedSeat }) {
+                pos.copy(state = DeckPositionState.OccupiedSeat)
+            } else pos
+            DeckPositionState.OccupiedSeat -> if (graph[pos]!!.count { it.state == DeckPositionState.OccupiedSeat } >= 4) {
+                pos.copy(state = DeckPositionState.EmptySeat)
+            } else pos
+            else -> pos
+        }
+    }
+}
+
+tailrec fun settleInSeats(layout: List<DeckPosition>): List<DeckPosition> {
+    val reseated = reseat(layout)
+    return if (reseated == layout) {
+        reseated
+    } else {
+        settleInSeats(reseated)
+    }
+}
+fun day11part1(puzzleInput: List<String>): Int {
+    return settleInSeats(parseInput(puzzleInput)).count { it.state == DeckPositionState.OccupiedSeat}
+}
+
+fun main() {
+    val puzzleInput = InputReader.lines(11)
+    println("Day 11, Part 1: ${day11part1(puzzleInput)}")
+}
+
--- src/main/kotlin/com/actualadam/aoc/aoc2020/worksheet/worksheet.ws.kts
+++ src/main/kotlin/com/actualadam/aoc/aoc2020/worksheet/worksheet.ws.kts
@@ -1,103 +1,79 @@
 package com.actualadam.aoc.aoc2020.worksheet
 
-import arrow.syntax.function.memoize
 import com.actualadam.aoc.aoc2020.InputReader
-import java.lang.Integer.sum
 
-val puzzleInput = InputReader.lines(10)
+val puzzleInput = InputReader.lines(11, "input.txt")
 puzzleInput
-val adapterBag = puzzleInput.map { it.toInt() }.sorted()
-adapterBag
 
-val startingJoltage = 0
-val deviceAdapterJoltage = adapterBag.maxOrNull()!! + 3
+data class DeckPosition(val row: Int, val col: Int, val state: DeckPositionState)
 
-fun findNextAdapter(adapters: List<Int>, curJoltage: Int): Int? {
-    val qualified = qualifiedNextAdapters(adapters, curJoltage)
-    return qualified.minOrNull()
+enum class DeckPositionState(val display: Char) {
+    OccupiedSeat('#'),
+    EmptySeat('L'),
+    Floor('.');
+
+    companion object {
+        val statesByDisplay = DeckPositionState.values().associateBy { it.display }
+        fun parse(display: Char) = statesByDisplay[display] ?: throw IllegalArgumentException("no state: $display")
+    }
 }
 
-fun qualifiedNextAdapters(adapters: List<Int>, curJoltage: Int) = adapters.filter { it - curJoltage in 1..3 }
+fun findByCoordinates(layout: List<DeckPosition>, row: Int, col: Int): DeckPosition? =
+    layout.find { it.row == row && it.col == col }
 
-fun buildGraph(adapters: List<Int>): Map<Int, List<Int>> {
-    val deviceAdapterJoltage = adapters.maxOrNull()!! + 3
-    val adapters = (adapters + 0 + deviceAdapterJoltage).sorted()
-    return adapters.map {
-        Pair(it, qualifiedNextAdapters(adapters, it))
+fun parseInput(input: List<String>): List<DeckPosition> {
+    return input.flatMapIndexed { rowNum, row ->
+        row.mapIndexed { colNum, charValue ->
+            DeckPosition(rowNum, colNum, DeckPositionState.parse(charValue))
+        }
+    }
+}
+
+fun buildGraph(layout: List<DeckPosition>): Map<DeckPosition, List<DeckPosition>> {
+    val maxRow = layout.maxByOrNull { it.row }
+    val maxCol = layout.maxByOrNull { it.col }
+    return layout.map {
+        val southRow = it.row + 1
+        val northRow = it.row - 1
+        val eastCol = it.col + 1
+        val westCol = it.col - 1
+
+        val adjacents = listOf(
+            findByCoordinates(layout, northRow, westCol),
+            findByCoordinates(layout, northRow, it.col),
+            findByCoordinates(layout, northRow, eastCol),
+            findByCoordinates(layout, it.row, westCol),
+            findByCoordinates(layout, it.row, eastCol),
+            findByCoordinates(layout, southRow, westCol),
+            findByCoordinates(layout, southRow, it.col),
+            findByCoordinates(layout, southRow, eastCol),
+        ).filterNotNull()
+        Pair(it, adjacents)
     }.toMap()
 }
 
-fun countJoltageDifferences(
-    adapters: List<Int> = adapterBag + deviceAdapterJoltage,
-    curJoltage: Int = startingJoltage,
-    acc: Pair<Int, Int> = Pair(0, 0)
-): Pair<Int, Int> {
-    if (adapters.isEmpty()) {
-        return acc
-    }
-    val nextAdapter = findNextAdapter(adapters, curJoltage)
-    val diff = nextAdapter!! - curJoltage
-    val nextAcc = when (diff) {
-        1 -> Pair(acc.first + 1, acc.second)
-        3 -> Pair(acc.first, acc.second + 1)
-        else -> acc
-    }
-    val nextBag = adapters.filter { it != nextAdapter }
-    return countJoltageDifferences(nextBag, nextAdapter, nextAcc)
-}
-
-val example1 = """
-   16
-   10
-   15
-   5
-   1
-   11
-   7
-   19
-   6
-   12
-   4
-""".trimIndent().split("\n").map { it.toInt() }
-
-val adapters = (example1 + 0 + (example1.maxOrNull()!! + 3)).sorted()
-
-adapters
-val adapter = 22
-// countPaths(22)
-example1
-val graph = buildGraph(example1)
-graph
-
-fun <K, V> transpose(graph: Map<K, List<V>>): Map<V, List<K>> {
-    return graph.flatMap { entry -> entry.value.map { Pair(it, entry.key) } } // flatten and value -> key swap
-        .groupBy { pair -> pair.first } // group pairs by new key
-        .mapValues { entry -> entry.value.map { it.second } } // just the 2nd, the first is redundant with the key
-}
-
-val transposed = transpose(graph)
-
-graph.keys.count()
-transposed.keys.count()
-transposed
-val pathCountByVertex = mutableMapOf<Int, Int>()
-
-val destination = example1.maxOrNull()!!
-val source = 0
-
-fun countPaths(graph: Map<Int, List<Int>>, from: Int, to: Int): Long {
-    var pathCount = 0L
-    fun helper(cur: Int) {
-        if (cur == to) {
-            pathCount++
-        } else {
-            graph[cur]!!.forEach {
-                helper(it)
-            }
+fun reseat(layout: List<DeckPosition>): List<DeckPosition> {
+    val graph = buildGraph(layout)
+    return layout.map { pos ->
+        when (pos.state) {
+            DeckPositionState.EmptySeat -> if (graph[pos]!!.none { it.state == DeckPositionState.OccupiedSeat }) {
+                pos.copy(state = DeckPositionState.OccupiedSeat)
+            } else pos
+            DeckPositionState.OccupiedSeat -> if (graph[pos]!!.count { it.state == DeckPositionState.OccupiedSeat } >= 4) {
+                pos.copy(state = DeckPositionState.EmptySeat)
+            } else pos
+            else -> pos
         }
     }
-    return pathCount
 }
 
-val memoizedCountPaths = { graph: Map<Int, List<Int>>, from: Int, to:Int -> countPaths(graph, from, to) }.memoize()
+tailrec fun settleInSeats(layout: List<DeckPosition>): List<DeckPosition> {
+    val reseated = reseat(layout)
+    return if (reseated == layout) {
+        reseated
+    } else {
+       settleInSeats(reseated)
+    }
+}
 
+settleInSeats(parseInput(puzzleInput)).count { it.state == DeckPositionState.OccupiedSeat}
